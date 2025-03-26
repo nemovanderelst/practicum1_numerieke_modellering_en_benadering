304ee3282818976ca2c37ac9e2799f1c7dc3f540
Rubensimons832 <ruben.simons@student.kuleuven.be>
2025-03-26 11:46:35 +0100

    verbeterd algoritme herhaalde Gram-Schmidt

--- herGS.m
+++ herGS.m
@@ -1,26 +1,23 @@
 function [Q, R] = herGS(A)
     [m, n] = size(A);
-    assert(m >= n);
     
     V = A;
-    W = eye(m, n);
-    Q = eye(m, n);
-    R = eye(n, n);
-    S = eye(m, n);
+    W = zeros(m, n);
+    Q = zeros(m, n);
+    R = zeros(n, n);
+    S = zeros(m, n);
+
+
+    for i = 1:n
+        R(i, i) = norm(V(:, i));
+        Q(:, i) = V(:, i) / R(i, i);
+        R(i, i+1:n) = Q(:, i)'*A(:, i+1:n);
+        V(:, i+1:n) = V(:, i+1:n) - Q(:, i)*R(i, i+1:n);
     
-    for j = 1:n
-        for i = 1:(j-1)
-            R(i, j) = Q(:, i)'*A(:, j);
-            V(:, j) = V(:, j) - R(i, j)*Q(:, i);
-        end    
-        
-        W(:, j) = V(:, j);
-        for i = 1:(j-1)
-            S(i, j) = Q(:, i)'*W(:, j);
-            V(:, j) = V(:, j) - S(i, j)*Q(:, i);
-            R(i, j) = R(i, j) + S(i, j);
-        end
-        R(j, j) = sqrt(V(:, j)'*V(:, j));
-        Q(:, j) = V(:, j) / R(j, j);
+        W(:, i) = V(:, i);
+        S(i, i+1:n) = Q(:, i)'*W(:, i+1:n);
+        V(:, i+1:n) = V(:, i+1:n) - Q(:, i)*S(i, i+1:n);
+        R(i, i+1:n) = R(i, i+1:n) + S(i, i+1:n);
     end
 end
+
